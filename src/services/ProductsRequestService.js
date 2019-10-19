68654031fef4f8eed093d632148b423540af9e82
Henrique <henriquekio@hotmail.com>
2019-10-19 19:51:22 -0400

    Finish http service to resource 'Products'

--- src/services/ProductsRequestService.js
+++ src/services/ProductsRequestService.js
@@ -1,7 +1,15 @@
 import { httpDefault } from '../helpers/http';
 
-const getAllProducts = () => httpDefault.get('products', { params: { cmd: 'list' } });
+const getAllProducts = () =>
+  httpDefault.get('products', { params: { cmd: 'list' } });
 
-// const getProducts = () =>
+const getProducts = (id = 0) =>
+  httpDefault.get('products', { params: { cmd: 'details', id } });
 
-export { getAllProducts };
+const createProducts = (params = {}) => httpDefault.post('products', ...params);
+
+const updateProducts = (params = {}, id = 0) => httpDefault.put(`products/${id}`, params);
+
+const deleteProducts = (params = {}, id = 0) => httpDefault.delete(`products/${id}`, params);
+
+export { getAllProducts, getProducts, createProducts, updateProducts, deleteProducts };
