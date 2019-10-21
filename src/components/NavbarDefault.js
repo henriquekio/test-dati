d3259e216d58aabdb4d6127f4abc203c3401eb6d
Henrique <henriquekio@hotmail.com>
2019-10-20 23:36:58 -0300

    fix responsive

--- src/components/NavbarDefault.js
+++ src/components/NavbarDefault.js
@@ -15,27 +15,65 @@ function NavbarDefault() {
       });
     }
 
+    const elemsNav = document.querySelectorAll('.sidenav');
+    // eslint-disable-next-line no-undef
+    const instanceNav = M.Sidenav.getInstance(elemsNav);
+
+    if (typeof instanceNav === 'undefined') {
+      // eslint-disable-next-line no-undef
+      M.Sidenav.init(elemsNav);
+    }
   }, []);
+
+  const closeNavbar = () => {
+    const elemsNav = document.querySelectorAll('.sidenav');
+    // eslint-disable-next-line no-undef
+    const instanceNav = M.Sidenav.getInstance(elemsNav[0]);
+    instanceNav.close();
+  };
 
   return (
     <>
       <nav className="navbar">
         <div className="nav-wrapper container">
-          <Link to="/" className="brand-logo">Logo</Link>
+          <Link to="/" className="brand-logo">
+            Dati - Shop
+          </Link>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a href="#" data-target="mobile-menu" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
-              <a className="dropdown-trigger" href="#!" data-target="produtos">Produtos<i
-                className="material-icons right">arrow_drop_down</i></a>
+              <a className="dropdown-trigger" href="#!" data-target="produtos">
+                Produtos<i className="material-icons right">arrow_drop_down</i>
+              </a>
             </li>
           </ul>
         </div>
       </nav>
       <ul id="produtos" className="dropdown-content">
         <li>
-          <NavLink className="blue-text" to='/'>Lista</NavLink>
+          <NavLink className="blue-text" to="/">
+            Lista
+          </NavLink>
         </li>
         <li>
-          <NavLink className="blue-text" to='/produtos/novo'>Novo Produto</NavLink>
+          <NavLink className="blue-text" to="/produtos/novo">
+            Novo Produto
+          </NavLink>
+        </li>
+      </ul>
+      <ul className="sidenav" id="mobile-menu">
+        <li onClick={closeNavbar}>
+          <NavLink className="white-text" to="/">
+            Lista
+          </NavLink>
+        </li>
+        <li onClick={closeNavbar}>
+          <NavLink className="white-text" to="/produtos/novo">
+            Novo Produto
+          </NavLink>
         </li>
       </ul>
     </>
--- src/views/Home/components/ListProducts.js
+++ src/views/Home/components/ListProducts.js
@@ -5,8 +5,7 @@ import { numberFormat } from '../../../helpers/helpers';
 import ProductDetails from './ProductDetails';
 import { updateProducts } from '../../../services/ProductsRequestService';
 
-
-const ListProducts = (props) => {
+const ListProducts = props => {
   const { products } = props;
 
   const [detail, setDetail] = useState('');
@@ -56,50 +55,63 @@ const ListProducts = (props) => {
 
   return (
     <ul className="list-products browser-default">
-      {
-        products.map((product, index) => (
-          <React.Fragment key={product.id}>
-            <li className={`list-products__item ${product.status === 'disable' ? 'disable' : ''}`}>
-              <p>
-                {product.short_description}
-                <br/>
-                <span className="grey-text lighten-3">{numberFormat('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL'
-                }, product.value)}</span>
-              </p>
-              <ul className="list-products__actions">
-                <li>
-                  <button onClick={() => toggleDetails(index)} type="button">
-                    <i className="material-icons blue-text">add</i>
-                  </button>
-                </li>
-                <li>
-                  <button onClick={() => deleteProduct(product)} type="button">
-                    <i className="material-icons red-text">delete</i>
-                  </button>
-                </li>
-                <li>
-                  <Link to={`/produtos/${product.id}`}>
-                    <i className="material-icons green-text">create</i>
-                  </Link>
-                </li>
-                <li>
-                  <div className="switch" onClick={() => changeStatus(product)}>
-                    <label>
-                      Inativo
-                      <input checked={product.status === 'enable'} type="checkbox"/>
-                      <span className="lever"/>
-                      Ativo
-                    </label>
-                  </div>
-                </li>
-              </ul>
-            </li>
-            {detail === index && (<ProductDetails {...{ product }} />)}
-          </React.Fragment>
-        ))
-      }
+      {products.map((product, index) => (
+        <React.Fragment key={product.id}>
+          <li
+            id={index === 0 ? 'first-item' : ''}
+            className={`list-products__item ${
+              product.status === 'disable' ? 'disable' : ''
+            }`}
+          >
+            <p>
+              {product.short_description}
+              <br />
+              <span className="grey-text lighten-3">
+                {numberFormat(
+                  'pt-BR',
+                  {
+                    style: 'currency',
+                    currency: 'BRL'
+                  },
+                  product.value
+                )}
+              </span>
+            </p>
+            <ul className="list-products__actions">
+              <li>
+                <button onClick={() => toggleDetails(index)} type="button">
+                  <i className="material-icons blue-text">add</i>
+                </button>
+              </li>
+              <li>
+                <button onClick={() => deleteProduct(product)} type="button">
+                  <i className="material-icons red-text">delete</i>
+                </button>
+              </li>
+              <li>
+                <Link to={`/produtos/${product.id}`}>
+                  <i className="material-icons green-text">create</i>
+                </Link>
+              </li>
+              <li>
+                {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+                <div className="switch" onClick={() => changeStatus(product)}>
+                  <label>
+                    Inativo
+                    <input
+                      defaultChecked={product.status === 'enable'}
+                      type="checkbox"
+                    />
+                    <span className="lever" />
+                    Ativo
+                  </label>
+                </div>
+              </li>
+            </ul>
+          </li>
+          {detail === index && <ProductDetails {...{ product }} />}
+        </React.Fragment>
+      ))}
     </ul>
   );
 };
